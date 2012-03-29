998eb977a9216a2e414d45567ec39e84dc30a27d
ashrithkulai@gmail.com <ashrithkulai@gmail.com@466a8e3a-6304-c9f0-05d0-8c5f28c1b440>
2012-03-29 23:58:55 +0000

    deleting. not necessary to be in project.

--- trunk/WebContent/SQL/referReadmeB4opening/autoload_auctionProduct.sql
+++
@@ -1,6 +0,0 @@
-LOAD DATA LOCAL INFILE 'E:\\auctionProduct.csv'
-INTO TABLE ebay.auctionProduct
-FIELDS TERMINATED BY '|' 
-ENCLOSED BY ""
-LINES TERMINATED BY '\r\n' 
-(auctionId, productId, basePrice, stepPrice);
--- trunk/WebContent/SQL/referReadmeB4opening/autoload_product.sql
+++
@@ -1,7 +0,0 @@
-LOAD DATA LOCAL INFILE 'E:\\product.csv'
-INTO TABLE ebay.product 
-FIELDS TERMINATED BY '|' 
-ENCLOSED BY ""
-LINES TERMINATED BY '\r\n' 
-(productId, subCategoryId, categoryId, productDesc, sellerId, saleType,
-prodCondition, startDate, endDate, shipmentState, shipmentCharges);
