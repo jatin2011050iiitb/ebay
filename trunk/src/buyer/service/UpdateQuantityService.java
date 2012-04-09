69358ff94f59ffa3543d5a383a19ae521ae9eefa
ashrithkulai@gmail.com <ashrithkulai@gmail.com@466a8e3a-6304-c9f0-05d0-8c5f28c1b440>
2012-04-09 22:37:29 +0000

    updating quantity is working. can increase or decrease quantity

---
+++ trunk/src/buyer/service/UpdateQuantityService.java
@@ -0,0 +1,50 @@
+package buyer.service;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import buyer.model.ShoppingCartItem;
+
+import ebay.util.DBconn;
+
+public class UpdateQuantityService {
+	private DBconn dbconn = null;
+	private Connection con = null;
+	private PreparedStatement pst = null;
+	private ResultSet resultSet1 = null;
+	private String query1 = null;
+	private int isSuccessful = 0;
+	
+	public ShoppingCartItem updateSingleProductQuantity(ShoppingCartItem shoppingCartItem, int quantity){
+		
+		int subtotal=0;
+		subtotal= shoppingCartItem.getUnitPrice()*quantity;
+		 try {
+				
+				query1 = "UPDATE shoppingCartItem SET subTotal=? , quantity=? where cartItemId=? and itemDeselected=?";
+				dbconn = new DBconn();
+				con = DBconn.getConnection();
+				pst = con.prepareStatement(query1);
+				pst.setInt(1, subtotal);
+				pst.setInt(2, quantity);
+				pst.setInt(3, shoppingCartItem.getCartItemId());
+				pst.setString(4,"0");
+				System.out.println(pst);
+				isSuccessful = pst.executeUpdate();
+			} catch (SQLException e) {
+				System.out.println(e.getMessage());
+			} finally {
+				dbconn.close(pst);
+				dbconn.close(con);
+			}
+			if(isSuccessful==1){
+		 shoppingCartItem.setSubtotal(subtotal); // this is short cut. you could also write a select query and set the sub total ;)
+			shoppingCartItem.setQuantity(quantity);
+			}
+		
+	return shoppingCartItem;
+	}
+
+}
