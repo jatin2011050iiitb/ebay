e7bc6378dcfd54388f0c18a0f7011e2b58a99e9f
anup.warnulkar@gmail.com <anup.warnulkar@gmail.com@466a8e3a-6304-c9f0-05d0-8c5f28c1b440>
2012-04-13 16:11:26 +0000

    Pending shipment confirmation by seller

---
+++ trunk/src/seller/action/ConfirmShipmentAction.java
@@ -0,0 +1,66 @@
+package seller.action;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import seller.model.ShipmentCart;
+import seller.service.ShipmentService;
+import user.model.User;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ConfirmShipmentAction extends ActionSupport {
+	private ShipmentCart shipmentCart;
+	private int cartId;
+	
+	private User user;
+	private int sellerId;
+	
+	private Map session;
+	
+	public String show() {
+		// get the id of the seller
+		session=ActionContext.getContext().getSession();
+		user = (User) session.get("objectuser");
+		sellerId = user.getUserId();
+		
+		ShipmentService shipmentService = new ShipmentService();
+
+			setShipmentCart(new ShipmentCart());
+			getShipmentCart().setCartId(getCartId());
+			getShipmentCart().setShipmentCartDetails(shipmentService.getShipmentCartDetails(getCartId()));
+			getShipmentCart().setShipmentItemList(shipmentService.getShipmentItems(getCartId()));
+
+		return "success";
+	}
+	
+	
+	public String execute() {
+		ShipmentService shipmentService = new ShipmentService();
+		shipmentService.addShippingDetails(getShipmentCart());
+		return "success";
+	}
+
+
+	public void setShipmentCart(ShipmentCart shipmentCart) {
+		this.shipmentCart = shipmentCart;
+	}
+
+
+	public ShipmentCart getShipmentCart() {
+		return shipmentCart;
+	}
+
+
+	public void setCartId(int cartId) {
+		this.cartId = cartId;
+	}
+
+
+	public int getCartId() {
+		return cartId;
+	}
+	
+	
+}
---
+++ trunk/src/seller/action/ShowPendingShipments.java
@@ -0,0 +1,38 @@
+package seller.action;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import seller.service.ShipmentService;
+import user.model.User;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ShowPendingShipments extends ActionSupport {
+	private ArrayList<Integer> cartIdList;
+	
+	private Map session;
+	private User user;
+	private int sellerId;
+	
+	public String execute() {
+		// get the id of the seller
+		session=ActionContext.getContext().getSession();
+		user = (User) session.get("objectuser");
+		sellerId = user.getUserId();
+		
+		ShipmentService shipmentService = new ShipmentService();
+		setCartIdList(shipmentService.getCartIdList(sellerId));
+		return "success";
+	}
+
+	public void setCartIdList(ArrayList<Integer> cartIdList) {
+		this.cartIdList = cartIdList;
+	}
+
+	public ArrayList<Integer> getCartIdList() {
+		return cartIdList;
+	}
+	
+}
