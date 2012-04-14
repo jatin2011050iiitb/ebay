3163dae4a5a894aeb0627416d46dd504ab1cb2cc
ashrithkulai@gmail.com <ashrithkulai@gmail.com@466a8e3a-6304-c9f0-05d0-8c5f28c1b440>
2012-04-14 22:38:54 +0000

    regular update

--- src/buyer/model/ShoppingCartArray.java
+++ src/buyer/model/ShoppingCartArray.java
@@ -2,9 +2,17 @@ package buyer.model;
 
 import java.util.ArrayList;
 
-public class ShoppingCartArray<ShoppingCart> {
-
+public class ShoppingCartArray {
 	
+	private ArrayList<ShoppingCart> shoppingCartList;
+
+	public ArrayList<ShoppingCart> getShoppingCartList() {
+		return shoppingCartList;
+	}
+
+	public void setShoppingCartList(ArrayList<ShoppingCart> shoppingCartList) {
+		this.shoppingCartList = shoppingCartList;
+	}
 	
 	
 }
