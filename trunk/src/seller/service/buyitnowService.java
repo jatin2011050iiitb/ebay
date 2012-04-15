5c7fcdfe6364e98b8f0f73ac98aba10868cee811
gaurav.vinter@gmail.com <gaurav.vinter@gmail.com@466a8e3a-6304-c9f0-05d0-8c5f28c1b440>
2012-04-15 00:08:59 +0000

    adding seller service.

---
+++ trunk/src/seller/service/buyitnowService.java
@@ -0,0 +1,261 @@
+package seller.service;
+
+import java.sql.*;
+import java.sql.Date;
+import java.util.*;
+import java.text.*;
+
+import com.opensymphony.xwork2.ActionContext;
+
+import seller.model.*;
+import user.model.User;
+
+import ebay.util.DBconn;
+
+public class buyitnowService {
+	private DBconn dbconn = null;
+	private Connection con = null;
+	private PreparedStatement pst = null;
+
+	private Statement stmt = null;
+	private ResultSet resultSet1 = null;
+	private ResultSet resultSet2 = null;
+	private ResultSet resultSet3 = null;
+	private String query1 = null;
+	private String query2 = null;
+	private String query3 = null;
+	private String query4 = null;
+	private String query5 = null;
+
+	private int categoryId;
+	private int subcategoryId;
+	private int productId;
+	private User user;
+	
+	public int getProductId(Product product) {
+		try {
+			
+				dbconn = new DBconn();
+				con = dbconn.getConnection();
+				
+				query4 = "SELECT productId from product where productDesc=?";
+				
+				pst = con.prepareStatement(query4);
+				
+				pst.setString(1, product.getProductDesc());
+				
+				resultSet3=pst.executeQuery();
+				
+				System.out.println(pst);
+			
+				if(resultSet3.next())
+				{
+					productId=resultSet3.getInt("productId");
+					System.out.println("product i.d is:"+productId);
+				}
+				return productId;
+			}
+
+			catch (Exception e) {
+				System.out.println(e.getMessage());
+				return 0;
+			}
+			
+			finally {
+				dbconn.close(resultSet3);
+				dbconn.close(pst);
+				dbconn.close(con);
+				
+			}
+	}
+	
+public void insert(Product product, BinProduct binproduct, String category, String subcategory ) {
+		
+	
+		try {
+			
+			Map session = ActionContext.getContext().getSession();
+			user =(User) session.get("objectuser");
+				
+				dbconn = new DBconn();
+				con = dbconn.getConnection();
+				
+				query1 = "SELECT categoryId from category where categoryDesc=?";
+				
+				pst = con.prepareStatement(query1);
+				pst.setString(1, category);
+				resultSet1=pst.executeQuery();
+				
+				System.out.println(pst);
+				
+				if(resultSet1.next())
+				{
+					categoryId=resultSet1.getInt("categoryId");
+					System.out.println("category i.d is:"+categoryId);
+				}
+
+			}
+
+			catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+			
+			finally {
+				dbconn.close(resultSet1);
+				dbconn.close(pst);
+				dbconn.close(con);
+			}
+
+			try {
+				
+				//to retrieve categoryId based on the category
+										
+					dbconn = new DBconn();
+					con = dbconn.getConnection();
+					
+					query2 = "SELECT subcategoryId from subcategory where subcategoryDesc=?";
+					
+					pst = con.prepareStatement(query2);
+					pst.setString(1, subcategory);
+					resultSet2=pst.executeQuery();
+					
+					System.out.println(pst);
+					
+					if(resultSet2.next())
+					{
+						subcategoryId=resultSet2.getInt("subcategoryId");
+						System.out.println("subcategory i.d is:"+subcategoryId);
+					}
+
+				}
+
+				catch (Exception e) {
+					System.out.println(e.getMessage());
+				}
+				
+				finally {
+					dbconn.close(resultSet2);
+					dbconn.close(pst);
+					dbconn.close(con);
+				}
+				
+				try {
+					
+					//to retrieve categoryId based on the category
+						
+						
+						dbconn = new DBconn();
+						con = dbconn.getConnection();
+						
+						query3 = "INSERT INTO product(subcategoryId, categoryId, productDesc, sellerId, saleType, prodCondition, startDate, endDate, shipmentState, shipmentCharges, sold) VALUES(?,?,?,?,?,?,?,?,?,?,?)";
+						
+						pst = con.prepareStatement(query3);
+//						System.out.println("subcategoryId="+subcategoryId);
+						pst.setInt(1, subcategoryId);
+//						System.out.println("categoryId="+categoryId);
+						pst.setInt(2, categoryId);
+//						System.out.println("product.getProductDesc()="+product.getProductDesc());
+						pst.setString(3, product.getProductDesc());
+//						System.out.println("user.getUserId()="+user.getUserId());
+						pst.setInt(4, user.getUserId());
+						
+						pst.setString(5, "1");
+//						System.out.println("product.getProdCondition()"+product.getProdCondition());
+						pst.setString(6, product.getProdCondition());
+						
+						pst.setTimestamp(7, null/*(Timestamp) product.getStartDate()*/);
+						pst.setTimestamp(8, null/*(Timestamp) product.getEndDate()*/);
+//						System.out.println("product.getShipmentState()="+product.getShipmentState());
+						pst.setString(9, product.getShipmentState());
+//						System.out.println("product.getShipmentCharges()="+product.getShipmentCharges());
+						pst.setInt(10, product.getShipmentCharges());
+						pst.setString(11, "0");
+						
+//						System.out.println("insert query="+pst);
+						pst.executeUpdate();
+						
+						System.out.println("insert query="+pst);
+											
+					}
+
+					catch (Exception e) {
+						System.out.println(e.getMessage());
+					}
+					
+					finally {
+						
+						dbconn.close(pst);
+						dbconn.close(con);
+//						System.out.println("third conn closed.");
+					}
+					
+					productId = getProductId(product);
+
+				/*try {
+					
+					//to retrieve categoryId based on the category
+												
+						dbconn = new DBconn();
+						con = dbconn.getConnection();
+						
+						query4 = "SELECT productId from product where productDesc=?";
+						
+						pst = con.prepareStatement(query4);
+						
+						pst.setString(1, product.getProductDesc());
+						
+						resultSet3=pst.executeQuery();
+						
+						System.out.println(pst);
+					
+						if(resultSet3.next())
+						{
+							productId=resultSet3.getInt("productId");
+							System.out.println("product i.d is:"+productId);
+						}
+					}
+
+					catch (Exception e) {
+						System.out.println(e.getMessage());
+					}
+					
+					finally {
+						
+						dbconn.close(pst);
+						dbconn.close(con);
+					}*/
+					
+					try {
+						
+						//to retrieve categoryId based on the category
+													
+							dbconn = new DBconn();
+							con = dbconn.getConnection();
+							
+							query5 = "INSERT INTO binproduct(productId, stock, binPrice) VALUES(?,?,?)";
+							
+							pst = con.prepareStatement(query5);
+							
+							pst.setInt(1, productId);
+							pst.setInt(2, binproduct.getStock());
+							pst.setInt(3, binproduct.getBinPrice());
+							
+							pst.executeUpdate();
+							
+							System.out.println(pst);
+														
+						}
+
+						catch (Exception e) {
+							System.out.println(e.getMessage());
+						}
+						
+						finally {
+							
+							dbconn.close(pst);
+							dbconn.close(con);
+						}
+
+		}//END OF THE METHOD
+
+}
