39e1b59eeda64bd462e1a5b59da7cfa4b574f855
gaurav.vinter@gmail.com <gaurav.vinter@gmail.com@466a8e3a-6304-c9f0-05d0-8c5f28c1b440>
2012-04-15 00:08:31 +0000

    adding seller models

---
+++ src/seller/model/BinProduct.java
@@ -0,0 +1,27 @@
+package seller.model;
+
+public class BinProduct {
+	private int productId;
+	private int stock;
+	private int binPrice;
+	
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	public int getBinPrice() {
+		return binPrice;
+	}
+	public void setBinPrice(int binPrice) {
+		this.binPrice = binPrice;
+	}
+		
+}
